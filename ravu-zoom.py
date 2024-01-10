1cc878351688d4a4adefe1cbe9a68af0d4c42e66
hauuau <52239673+hauuau@users.noreply.github.com>
2024-01-10 09:36:56 +0900

    ravu-zoom.py: fix a bug in ravu-zoom
    Ref: Blinue/Magpie#516

--- ravu-zoom.py
+++ ravu-zoom.py
@@ -410,7 +410,7 @@ for (int id = int(gl_LocalInvocationIndex); id < rect.x * rect.y; id += int(gl_W
         self.function_header_compute()
 
         GLSL("ivec2 group_begin = ivec2(gl_WorkGroupID) * ivec2(gl_WorkGroupSize);")
-        GLSL("ivec2 group_end = group_begin + ivec2(gl_WorkGroupSize) - ivec2(1, 1);")
+        GLSL("ivec2 group_end = group_begin + ivec2(gl_WorkGroupSize);")
         GLSL("ivec2 rectl = ivec2(floor(HOOKED_size * HOOKED_map(group_begin) - 0.5)) - %d;" % (self.radius - 1))
         GLSL("ivec2 rectr = ivec2(floor(HOOKED_size * HOOKED_map(group_end) - 0.5)) + %d;" % self.radius)
         GLSL("ivec2 rect = rectr - rectl + 1;")
